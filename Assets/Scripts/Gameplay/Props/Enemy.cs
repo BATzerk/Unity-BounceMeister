3f1ab7733e2eda5f720ebba53861bc63b8bc3d2a
BATzerk <batzerk@gmail.com>
2018-05-30 16:45:37 -0400

    +Way basic enemy (non-interactive)

---
+++ Assets/Scripts/Gameplay/Props/Enemy.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	// Constants
+	private const float FrictionAir = 0.6f;
+	private const float FrictionGround = 0.6f;
+	private const float InputScaleX = 0.08f;
+	private Vector2 gravity = new Vector2(0, -0.05f);
+	// Properties
+	private bool[] onGrounds; // index is side.
+	private Vector2 vel;
+	private Vector2 size;
+	private Vector2 inputAxis; // QUICK IMPLEMENTATION for getting these dudies to movies.
+	// Components
+//	[SerializeField] private PlayerBody myBody=null;
+	[SerializeField] private EnemyWhiskers myWhiskers=null;
+	[SerializeField] private BoxCollider2D bodyCollider=null;
+
+	// Getters/Setters
+	public Vector2 Pos { get { return pos; } }
+	public Vector2 Vel { get { return vel; } }
+	public Vector2 Size { get { return size; } }
+//	public bool OnGround { get { return onGround; } }
+
+	private bool feetOnGround { get { return onGrounds[Sides.B]; } }
+	private Vector2 pos {
+		get { return this.transform.localPosition; }
+		set { this.transform.localPosition = value; }
+	}
+	private Vector2 GetAppliedVel() {
+		Vector2 av = vel;
+		float distL = myWhiskers.GroundDistMin(Sides.L);
+		float distR = myWhiskers.GroundDistMin(Sides.R);
+		float distB = myWhiskers.GroundDistMin(Sides.B);
+		float distT = myWhiskers.GroundDistMin(Sides.T);
+		// Clamp our vel so we don't intersect anything.
+		if (vel.x<0 && vel.x<-distL) {
+			av = new Vector2(-distL, av.y);
+		}
+		else if (vel.x>0 && vel.x>distR) {
+			av = new Vector2(distR, av.y);
+		}
+		if (vel.y<0 && vel.y<-distB) {
+			av = new Vector2(av.x, -distB);
+		}
+		else if (vel.y>0 && vel.y>distT) {
+			av = new Vector2(av.x, distT);
+		}
+		return av;
+	}
+
+
+
+	// ----------------------------------------------------------------
+	//  Start
+	// ----------------------------------------------------------------
+	private void Start () {
+		onGrounds = new bool[4];
+
+		inputAxis = new Vector2(1f, 0);
+
+		// Size me, queen!
+		SetSize (new Vector2(2.5f, 2.5f)); // NOTE: I don't understand why we gotta cut it by 100x. :P
+
+		vel = Vector2.zero;
+	}
+	private void SetSize(Vector2 _size) {
+		this.size = _size;
+//		myBody.SetSize(this.size);
+		bodyCollider.size = _size;
+	}
+
+
+
+	// ----------------------------------------------------------------
+	//  FixedUpdate
+	// ----------------------------------------------------------------
+	private void FixedUpdate () {
+		if (Time.timeScale == 0) { return; } // No time? No dice.
+		Vector2 ppos = pos;
+
+		UpdateOnGrounds();
+		ApplyFriction();
+		ApplyGravity();
+		AcceptDirectionalInput();
+		myWhiskers.UpdateGroundDists(); // update these dependently now, so we guarantee most up-to-date info.
+		ApplyVel();
+
+		// Update vel to be the distance we ended up moving this frame.
+		vel = pos - ppos;
+	}
+
+	private void ApplyGravity() {
+		vel += gravity;
+	}
+	private void AcceptDirectionalInput() {
+		if (InputController.Instance==null) { return; } // for building at runtime.
+
+		// Moving horizontally!
+		if (inputAxis.x != 0) {
+			float velXDelta = inputAxis.x*InputScaleX;
+			vel += new Vector2(velXDelta, 0);
+		}
+	}
+	private void ApplyFriction() {
+		if (feetOnGround) {
+			vel = new Vector2(vel.x*FrictionGround, vel.y);
+		}
+		else {
+			vel = new Vector2(vel.x*FrictionAir, vel.y);
+		}
+	}
+	private void ApplyVel() {
+		if (vel != Vector2.zero) {
+			Vector2 appliedVel = GetAppliedVel();
+			pos += appliedVel;
+		}
+	}
+
+	private void UpdateOnGrounds() {
+		for (int side=0; side<EnemyWhiskers.NumSides; side++) {
+			Collider2D ground = myWhiskers.GetGroundTouching(side);
+			float sideSpeed = GetSideSpeed(side);
+			bool touchingGround = ground!=null;//QQQ test && sideSpeed>=0; // I'm "touching" this ground if it exists and I'm not moving *away* from it!
+			if (onGrounds[side] && !touchingGround) {
+				OnLeaveGround(side);
+			}
+			else if (!onGrounds[side] && touchingGround) {
+				OnTouchGround(side, ground);
+			}
+		}
+	}
+	/** Returns the relative speed we're traveling in at this side. */
+	private float GetSideSpeed(int side) {
+		switch (side) {
+		case Sides.L: return -vel.x;
+		case Sides.R: return  vel.x;
+		case Sides.B: return -vel.y;
+		case Sides.T: return  vel.y;
+		default: Debug.LogError("Side not recognized: " + side); return 0;
+		}
+	}
+
+
+	// ----------------------------------------------------------------
+	//  Doers
+	// ----------------------------------------------------------------
+//	private void Jump() {
+//		vel = new Vector2(vel.x, JumpForce);
+//	}
+
+
+	// ----------------------------------------------------------------
+	//  Events (Physics)
+	// ----------------------------------------------------------------
+	private void OnLeaveGround(int side) {
+		onGrounds[side] = false;
+	}
+	private void OnTouchGround(int side, Collider2D groundCol) {
+		onGrounds[side] = true;
+
+		// A wall?? Reverse my horz direction!
+		if (side==Sides.L || side==Sides.R) {
+			inputAxis = new Vector2(-inputAxis.x, inputAxis.y);
+		}
+
+		// Inform the ground!
+//		Collidable collidable = groundCol.GetComponent<Collidable>();
+//		if (collidable != null) {
+//			collidable.OnCollideWithPlayer(this);
+//		}
+	}
+
+
+
+}
+
---
+++ Assets/Scripts/Gameplay/Props/EnemyWhiskers.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWhiskers : MonoBehaviour {
+	// Constants
+	public const int NumSides = 4; // it's hip to be square.
+	private const int NumWhiskersPerSide = 3; // this MUST match SideOffsetLocs! Just made its own variable for easy/readable access.
+	private float[] SideOffsetLocs = new float[]{-0.45f, 0f, 0.45f}; // 3 whiskers per side: left, center, right.
+	// References
+	[SerializeField] private Enemy myEnemy=null;
+	// Properties
+	[SerializeField] LayerMask lm_ground; // All sides always care about Ground.
+	[SerializeField] LayerMask lm_platform; // Only the bottom side cares about Platforms.
+	private Collider2D[,] collidersAroundMe; // by side,index.
+	private float[,] groundDists; // by side,index. This is *all* whisker data.
+	private int[] minDistsIndexes; // by side. WHICH whisker at this side is the closest!
+	private RaycastHit2D hit; // only out here so we don't have to make a ton every frame.
+	private Vector2[] whiskerDirs;
+
+	// Getters
+	private Vector2 playerSize { get { return myEnemy.Size; } }
+	private Vector2 WhiskerPos(int side, int index) {
+		Vector2 pos = myEnemy.Pos;
+		float sideOffsetLoc = SideOffsetLocs[index];
+		if (side==Sides.L || side==Sides.R) {
+			pos += new Vector2(whiskerDirs[side].x*playerSize.x*0.5f, playerSize.y*sideOffsetLoc);
+		}
+		else {
+			pos += new Vector2(playerSize.x*sideOffsetLoc, whiskerDirs[side].y*playerSize.y*0.5f);
+		}
+		return pos;
+	}
+	/** It's most efficient only to search as far as the Player is going to move this frame. */
+	private float GetRaycastSearchDist(int side) {
+		const float bloat = 0.2f; // how much farther than the player's exact velocity to look. For safety.
+		switch (side) {
+		case Sides.L: return Mathf.Max(0, -myEnemy.Vel.x) + bloat;
+		case Sides.R: return Mathf.Max(0,  myEnemy.Vel.x) + bloat;
+		case Sides.B: return Mathf.Max(0, -myEnemy.Vel.y) + bloat;
+		case Sides.T: return Mathf.Max(0,  myEnemy.Vel.y) + bloat;
+		default: Debug.LogError("Side undefined in GetRaycastSearchDist!: " + side); return 0;
+		}
+	}
+	private LayerMask GetLayerMask(int side) {
+		if (side == Sides.B) { return lm_ground | lm_platform; } // Bottom side? Return ground AND platforms!
+		return lm_ground; // All other sides only care about ground.
+	}
+
+	public float GroundDistMin(int side) {
+		if (minDistsIndexes[side] == -1) { return Mathf.Infinity; } // No closest whisker (none collide)? They're all infinity, then.
+		return groundDists[side, minDistsIndexes[side]];
+	}
+	public Collider2D GetGroundTouching(int side) {
+		if (collidersAroundMe==null) { return null; } // Safety check for runtime compile.
+		UpdateGroundDist(side); // Just update the bottom.
+		if (minDistsIndexes[side] == -1) { return null; } // No closest whisker (none collide)? Return null.
+		return collidersAroundMe[side, minDistsIndexes[Sides.B]];
+	}
+
+
+	// ----------------------------------------------------------------
+	//  Gizmos!
+	// ----------------------------------------------------------------
+	void OnDrawGizmos() {
+		if (whiskerDirs==null || groundDists==null) { return; } // Safety check.
+
+		for (int side=0; side<whiskerDirs.Length; side++) {
+			float length = GetRaycastSearchDist(side);
+			Vector2 dir = whiskerDirs[side];
+			for (int index=0; index<NumWhiskersPerSide; index++) {
+				Vector2 startPos = WhiskerPos(side, index);
+				bool isTouchingGround = groundDists[side,index] < 0.1f;
+				Gizmos.color = isTouchingGround ? Color.green : Color.red;
+				Gizmos.DrawLine(startPos, startPos + dir * length);
+			}
+		}
+	}
+
+
+
+	// ----------------------------------------------------------------
+	//  Start
+	// ----------------------------------------------------------------
+	private void Awake() {
+		groundDists = new float[NumSides,NumWhiskersPerSide];
+		collidersAroundMe = new Collider2D[NumSides,NumWhiskersPerSide];
+		minDistsIndexes = new int[NumSides];
+		whiskerDirs = new Vector2[NumSides];
+		whiskerDirs[Sides.L] = Vector2Int.L.ToVector2();
+		whiskerDirs[Sides.R] = Vector2Int.R.ToVector2();
+		whiskerDirs[Sides.T] = Vector2Int.T.ToVector2();
+		whiskerDirs[Sides.B] = Vector2Int.B.ToVector2();
+	}
+	private void Start() {
+		UpdateGroundDists(); // Just for consistency.
+	}
+
+
+
+	// ----------------------------------------------------------------
+	//  Update
+	// ----------------------------------------------------------------
+	//	private void FixedUpdate() {
+	//		UpdateGroundDists();
+	//	}
+	public void UpdateGroundDists() {
+		for (int i=0; i<whiskerDirs.Length; i ++) {
+			UpdateGroundDist(i);
+		}
+	}
+	public void UpdateGroundDist(int side) {
+		if (groundDists==null) { return; } // Safety check (for runtime compile).
+		minDistsIndexes[side] = -1; // Default this to -1: There is no closest, because they're all infinity.
+		for (int index=0; index<NumWhiskersPerSide; index++) {
+			UpdateWhiskerRaycast(side, index); // update the distances and colliders.
+			float dist = groundDists[side,index]; // use the dist we just updated.
+			if (GroundDistMin(side) > dist) { // Update the min distance, too.
+				minDistsIndexes[side] = index;
+			}
+		}
+	}
+	private void UpdateWhiskerRaycast(int side, int index) {
+		Vector2 dir = whiskerDirs[side];
+		Vector2 pos = WhiskerPos(side, index);
+		float raycastSearchDist = GetRaycastSearchDist(side);
+		LayerMask mask = GetLayerMask(side);
+		hit = Physics2D.Raycast(pos, dir, raycastSearchDist, mask);
+		float dist = Mathf.Infinity; // default to infinity in case we don't hit any ground.
+		if (hit.collider != null) {
+			dist = Vector2.Distance(hit.point, pos);
+		}
+		groundDists[side,index] = dist;
+		collidersAroundMe[side,index] = hit.collider;
+	}
+
+
+
+}
+
+
+//			if (LayerMask.LayerToName(hit.collider.gameObject.layer) == LayerNames.Ground) {
+//			}
+
+//	public bool IsTouchingGround() {
+////		UpdateGroundDists();
+//		for (int i=0; i<NumSides; i ++) {
+//			if (IsTouchingGroundAtSide(i)) { return true; }
+//		}
+//		return false;
+//	}
+//	public bool IsTouchingGroundAtSide(Vector2 dir) {
+//		return IsTouchingGroundAtSide(dir.ToVector2Int());
+//	}
+//	public bool IsTouchingGroundAtSide(Vector2Int dir) {
+//		return IsTouchingGroundAtSide(MathUtils.GetSide(dir));
+//	}
+//	public bool IsTouchingGroundAtSide(int side) {
+//		return DistToGroundAtSide(side) < 0.3f;
+//	}
+//	public float DistToGroundAtSide(int side) {
+//		Vector2 dir = whiskerDirs[side];
+//		Vector2 pos = WhiskerPos(side);
+//		hit = Physics2D.Raycast(pos, dir, Mathf.Infinity, myLayerMask);
+//		if (hit.collider != null && !hit.collider.isTrigger) {
+//			float dist = Vector2.Distance(hit.point, pos);
+//			if (LayerMask.LayerToName(hit.collider.gameObject.layer) == LayerNames.Ground) {
+//				return dist;
+//			}
+//		}
+//		return Mathf.Infinity;
+//	}
